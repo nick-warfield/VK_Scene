225d3d09f5f19170a1b0192c8f895508e73dcd71
Nicholas K Warfield <nick@nkwarfield.io>
2020-10-14 12:48:15 -0700

    Finished Physical Device setup + added autocomplete file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <algorithm>
+#include <optional>
 #include <vector>
 #include <iostream>
 #include <stdexcept>
@@ -49,6 +49,32 @@ void DestroyDebugUtilsMessengerEXT(
 	}
 }
 
+struct QueueFamilyIndices {
+	std::optional<uint32_t> graphicsFamily;
+
+	bool isComplete() { return graphicsFamily.has_value(); }
+};
+QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+	QueueFamilyIndices indices;
+
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+	int i = 0;
+	for (const auto& queueFamily : queueFamilies) {
+		if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			indices.graphicsFamily = i;
+
+			if (indices.isComplete()) break;
+		}
+		i++;
+	}
+	return indices;
+}
+
 class HelloTriangleApplication {
 public:
     void run() {
@@ -75,7 +101,43 @@ private:
     void initVulkan() {
 		createInstance();
 		setupDebugMessenger();
+		pickPhysicalDevice();
     }
+
+	void pickPhysicalDevice() {
+		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+		if (deviceCount == 0) {
+			throw std::runtime_error("failed to find GPUs with Vulkan Support");
+		}
+
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+		for (const auto& dev : devices) {
+			if (isDeviceSuitable(dev)) {
+				physicalDevice = dev;
+				break;
+			}
+		}
+
+		if (physicalDevice == VK_NULL_HANDLE) {
+			throw std::runtime_error("failed to find suitable GPU");
+		}
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device) {
+		QueueFamilyIndices indices = findQueueFamilies(device);
+		return indices.isComplete();
+
+//		// One possible implementation, but we only need vulkan support
+//		VkPhysicalDeviceProperties deviceProperties;
+//		VkPhysicalDeviceFeatures deviceFeatures;
+//		vkGetPhysicalDeviceProperties(device, &deviceProperties);
+//		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+//		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU
+//			&& deviceFeatures.geometryShader;
+	}
 
 	void populateDebugMessengerCreateInfo(
 			VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
