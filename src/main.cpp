25e721c22f3d3f7ce9fda6aeadd7c0e8b48dc227
Nicholas K Warfield <nick@nkwarfield.io>
2020-10-16 17:15:46 -0700

    Implemented an Index Buffer and use it to draw

--- src/main.cpp
+++ src/main.cpp
@@ -107,11 +107,16 @@ struct Vertex {
 	}
 };
 
-// triangle with normalized device coordinates
+// rectangle with normalized device coordinates
 const std::vector<Vertex> vertices = {
-	{{ 0.0f, -0.5f }, { 1.0f, 1.0f, 1.0f }},
-	{{ 0.5f,  0.5f }, { 0.0f, 1.0f, 0.0f }},
-	{{ -0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f }}
+	{{ -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
+    {{  0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }},
+    {{  0.5f,  0.5f }, { 0.0f, 0.0f, 1.0f }},
+    {{ -0.5f,  0.5f }, { 1.0f, 1.0f, 1.0f }}
+};
+const std::vector<uint16_t> indices = {
+	0, 1, 2,
+	2, 3, 0
 };
 
 class HelloTriangleApplication {
@@ -153,6 +158,8 @@ private:
 	bool framebufferResized = false;
 	VkBuffer vertexBuffer;
 	VkDeviceMemory vertexBufferMemory;
+	VkBuffer indexBuffer;
+	VkDeviceMemory indexBufferMemory;
 
 	void initWindow() {
 		glfwInit();
@@ -178,9 +185,39 @@ private:
 		createFramebuffers();
 		createCommandPool();
 		createVertexBuffer();
+		createIndexBuffer();
 		createCommandBuffers();
 		createSyncObjects();
     }
+
+	void createIndexBuffer() {
+		VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		createBuffer(
+				bufferSize,
+				VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+				VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+				stagingBuffer,
+				stagingBufferMemory);
+
+		void *data;
+		vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), (size_t)bufferSize);
+		vkUnmapMemory(device, stagingBufferMemory);
+
+		createBuffer(
+				bufferSize,
+				VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+				VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+				indexBuffer,
+				indexBufferMemory);
+		copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+		vkDestroyBuffer(device, stagingBuffer, nullptr);
+		vkFreeMemory(device, stagingBufferMemory, nullptr);
+	}
 
 	void createBuffer(
 			VkDeviceSize size,
@@ -370,9 +407,10 @@ private:
 			VkBuffer vertexBuffers[] = { vertexBuffer };
 			VkDeviceSize offsets[] = { 0 };
 			vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
+			vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
 
 			// (_, vertex count, instance count, first vertex, first instance)
-			vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+			vkCmdDrawIndexed(commandBuffers[i], static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 			vkCmdEndRenderPass(commandBuffers[i]);
 
 			if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
@@ -1097,6 +1135,9 @@ private:
 
     void cleanup() {
 		cleanupSwapChain();
+
+		vkDestroyBuffer(device, indexBuffer, nullptr);
+		vkFreeMemory(device, indexBufferMemory, nullptr);
 
 		vkDestroyBuffer(device, vertexBuffer, nullptr);
 		vkFreeMemory(device, vertexBufferMemory, nullptr);
