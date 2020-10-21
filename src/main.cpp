50935e4d549675d0a92d6068a16fdcadb1eec294
Nicholas K Warfield <nick@nkwarfield.io>
2020-10-20 23:11:44 -0700

    Generated and used mipmaps

--- src/main.cpp
+++ src/main.cpp
@@ -104,9 +104,10 @@ struct Vertex {
     glm::vec3 color;
     glm::vec2 texCoord;
 
-	bool operator==(const Vertex& other) const {
-		return pos == other.pos && color == other.color && texCoord == other.texCoord;
-	}
+    bool operator==(const Vertex& other) const
+    {
+	return pos == other.pos && color == other.color && texCoord == other.texCoord;
+    }
 
     static VkVertexInputBindingDescription getBindingDescription()
     {
@@ -141,13 +142,13 @@ struct Vertex {
 };
 
 namespace std {
-	template<> struct hash<Vertex> {
-		size_t operator()(Vertex const& vertex) const {
-			return ((hash<glm::vec3>()(vertex.pos) ^
-					(hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
-					(hash<glm::vec2>()(vertex.texCoord) << 1);
-		}
-	};
+template <>
+struct hash<Vertex> {
+    size_t operator()(Vertex const& vertex) const
+    {
+	return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
+    }
+};
 }
 
 struct UniformBufferObject {
@@ -204,6 +205,7 @@ private:
     std::vector<VkDeviceMemory> uniformBuffersMemory;
     VkDescriptorPool descriptorPool;
     std::vector<VkDescriptorSet> descriptorSets;
+    uint32_t mipLevels;
     VkImage textureImage;
     VkDeviceMemory textureImageMemory;
     VkImageView textureImageView;
@@ -211,10 +213,10 @@ private:
     VkImage depthImage;
     VkDeviceMemory depthImageMemory;
     VkImageView depthImageView;
-	std::vector<Vertex> vertices;
-	std::vector<uint32_t> indices;
-	VkBuffer vertexBuffer;
-	VkDeviceMemory vertexBufferMemory;
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory;
 
     void initWindow()
     {
@@ -256,44 +258,45 @@ private:
 	createSyncObjects();
     }
 
-	void loadModel() {
-		tinyobj::attrib_t attrib;
-		std::vector<tinyobj::shape_t> shapes;
-		std::vector<tinyobj::material_t> materials;
-		std::string err;
+    void loadModel()
+    {
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string err;
 
-		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, MODEL_PATH.c_str())) {
-			throw std::runtime_error(err);
-		}
-
-		std::unordered_map<Vertex, uint32_t> uniqueVertices;
-
-		for (const auto& shape : shapes) {
-			for (const auto& index : shape.mesh.indices) {
-				Vertex vertex{};
-
-				vertex.pos = {
-					attrib.vertices[3 * index.vertex_index + 0],
-					attrib.vertices[3 * index.vertex_index + 1],
-					attrib.vertices[3 * index.vertex_index + 2]
-				};
-
-				vertex.texCoord = {
-					attrib.texcoords[2 * index.texcoord_index + 0],
-					1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
-				};
-
-				vertex.color = { 1.0f, 1.0f, 1.0f };
-
-				if (uniqueVertices.count(vertex) == 0) {
-					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
-					vertices.push_back(vertex);
-				}
-
-				indices.push_back(uniqueVertices[vertex]);
-			}
-		}
+	if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, MODEL_PATH.c_str())) {
+	    throw std::runtime_error(err);
 	}
+
+	std::unordered_map<Vertex, uint32_t> uniqueVertices;
+
+	for (const auto& shape : shapes) {
+	    for (const auto& index : shape.mesh.indices) {
+		Vertex vertex {};
+
+		vertex.pos = {
+		    attrib.vertices[3 * index.vertex_index + 0],
+		    attrib.vertices[3 * index.vertex_index + 1],
+		    attrib.vertices[3 * index.vertex_index + 2]
+		};
+
+		vertex.texCoord = {
+		    attrib.texcoords[2 * index.texcoord_index + 0],
+		    1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+		};
+
+		vertex.color = { 1.0f, 1.0f, 1.0f };
+
+		if (uniqueVertices.count(vertex) == 0) {
+		    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+		    vertices.push_back(vertex);
+		}
+
+		indices.push_back(uniqueVertices[vertex]);
+	    }
+	}
+    }
 
     void createDepthResources()
     {
@@ -301,19 +304,21 @@ private:
 	createImage(
 	    swapChainExtent.width,
 	    swapChainExtent.height,
+	    1,
 	    depthFormat,
 	    VK_IMAGE_TILING_OPTIMAL,
 	    VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
 	    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 	    depthImage,
 	    depthImageMemory);
-	depthImageView = createImageView(depthImage, depthFormat, VK_IMAGE_ASPECT_DEPTH_BIT);
+	depthImageView = createImageView(depthImage, depthFormat, VK_IMAGE_ASPECT_DEPTH_BIT, 1);
 
 	transitionImageLayout(
 	    depthImage,
 	    depthFormat,
 	    VK_IMAGE_LAYOUT_UNDEFINED,
-	    VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
+	    VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+	    1);
     }
 
     bool hasStencilComponent(VkFormat format)
@@ -377,10 +382,10 @@ private:
 
     void createTextureImageView()
     {
-	textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT);
+	textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT, mipLevels);
     }
 
-    VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags)
+    VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t mipLevels)
     {
 	VkImageViewCreateInfo viewInfo {};
 	viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
@@ -389,7 +394,7 @@ private:
 	viewInfo.format = format;
 	viewInfo.subresourceRange.aspectMask = aspectFlags;
 	viewInfo.subresourceRange.baseMipLevel = 0;
-	viewInfo.subresourceRange.levelCount = 1;
+	viewInfo.subresourceRange.levelCount = mipLevels;
 	viewInfo.subresourceRange.baseArrayLayer = 0;
 	viewInfo.subresourceRange.layerCount = 1;
 
@@ -414,6 +419,8 @@ private:
 	if (!pixels) {
 	    throw std::runtime_error("failed to load texture image");
 	}
+
+	mipLevels = static_cast<uint32_t>(std::floor(std::log2(std::max(texWidth, texHeight)))) + 1;
 	VkDeviceSize imageSize = texWidth * texWidth * 4;
 
 	VkBuffer stagingBuffer;
@@ -434,9 +441,10 @@ private:
 	createImage(
 	    texWidth,
 	    texHeight,
+	    mipLevels,
 	    VK_FORMAT_R8G8B8A8_SRGB,
 	    VK_IMAGE_TILING_OPTIMAL,
-	    VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+	    VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
 	    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 	    textureImage,
 	    textureImageMemory);
@@ -445,7 +453,8 @@ private:
 	    textureImage,
 	    VK_FORMAT_R8G8B8A8_SRGB,
 	    VK_IMAGE_LAYOUT_UNDEFINED,
-	    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+	    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+	    mipLevels);
 
 	copyBufferToImage(
 	    stagingBuffer,
@@ -453,19 +462,121 @@ private:
 	    static_cast<uint32_t>(texWidth),
 	    static_cast<uint32_t>(texHeight));
 
-	transitionImageLayout(
-	    textureImage,
-	    VK_FORMAT_R8G8B8A8_SRGB,
-	    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
-	    VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+	//	transitionImageLayout(
+	//	    textureImage,
+	//	    VK_FORMAT_R8G8B8A8_SRGB,
+	//	    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+	//	    VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+	//		mipLevels);
+
+	generateMipmaps(textureImage, VK_FORMAT_R8G8B8A8_SRGB, texWidth, texHeight, mipLevels);
 
 	vkDestroyBuffer(device, stagingBuffer, nullptr);
 	vkFreeMemory(device, stagingBufferMemory, nullptr);
     }
 
+    void generateMipmaps(
+	VkImage image,
+	VkFormat imageFormat,
+	int32_t texWidth,
+	int32_t texHeight,
+	int32_t mipLevels)
+    {
+	VkFormatProperties formatProperties;
+	vkGetPhysicalDeviceFormatProperties(physicalDevice, imageFormat, &formatProperties);
+
+	if (!(formatProperties.optimalTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT)) {
+		throw std::runtime_error("texture image format does not support linear blitting");
+	}
+
+	VkCommandBuffer commandBuffer = beginSingleTimeCommands();
+
+	VkImageMemoryBarrier barrier {};
+	barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+	barrier.image = image;
+	barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+	barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+	barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	barrier.subresourceRange.baseArrayLayer = 0;
+	barrier.subresourceRange.layerCount = 1;
+	barrier.subresourceRange.levelCount = 1;
+
+	int32_t mipWidth = texWidth;
+	int32_t mipHeight = texHeight;
+
+	for (uint32_t i = 1; i < (uint32_t)mipLevels; ++i) {
+	    barrier.subresourceRange.baseMipLevel = i - 1;
+	    barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+	    barrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+	    barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+	    barrier.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+
+	    vkCmdPipelineBarrier(
+		commandBuffer,
+		VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0,
+		0, nullptr,
+		0, nullptr,
+		1, &barrier);
+
+	    VkImageBlit blit {};
+	    blit.srcOffsets[0] = { 0, 0, 0 };
+	    blit.srcOffsets[1] = { mipWidth, mipHeight, 1 };
+	    blit.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	    blit.srcSubresource.mipLevel = i - 1;
+	    blit.srcSubresource.baseArrayLayer = 0;
+	    blit.srcSubresource.layerCount = 1;
+	    blit.dstOffsets[0] = { 0, 0, 0 };
+	    blit.dstOffsets[1] = { mipWidth > 1 ? mipWidth / 2 : 1, mipHeight > 1 ? mipHeight / 2 : 1, 1 };
+	    blit.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	    blit.dstSubresource.mipLevel = i;
+	    blit.dstSubresource.baseArrayLayer = 0;
+	    blit.dstSubresource.layerCount = 1;
+
+	    vkCmdBlitImage(
+		commandBuffer,
+		image, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+		image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+		1, &blit,
+		VK_FILTER_LINEAR);
+
+	    barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+	    barrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+	    barrier.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+	    barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+	    vkCmdPipelineBarrier(
+		commandBuffer,
+		VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0,
+		0, nullptr,
+		0, nullptr,
+		1, &barrier);
+
+	    if (mipWidth > 1)
+		mipWidth /= 2;
+	    if (mipHeight > 1)
+		mipHeight /= 2;
+	}
+
+	barrier.subresourceRange.baseMipLevel = mipLevels - 1;
+	barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+	barrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+	barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+	barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+	vkCmdPipelineBarrier(
+	    commandBuffer,
+	    VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0,
+	    0, nullptr,
+	    0, nullptr,
+	    1, &barrier);
+
+	endSingleTimeCommands(commandBuffer);
+    }
+
     void createImage(
 	uint32_t width,
 	uint32_t height,
+	uint32_t mipLevels,
 	VkFormat format,
 	VkImageTiling tiling,
 	VkImageUsageFlags usage,
@@ -479,7 +590,7 @@ private:
 	imageInfo.extent.width = width;
 	imageInfo.extent.height = height;
 	imageInfo.extent.depth = 1;
-	imageInfo.mipLevels = 1;
+	imageInfo.mipLevels = mipLevels;
 	imageInfo.arrayLayers = 1;
 	imageInfo.format = format;
 	imageInfo.tiling = tiling;
@@ -514,7 +625,8 @@ private:
 	VkImage image,
 	VkFormat format,
 	VkImageLayout oldLayout,
-	VkImageLayout newLayout)
+	VkImageLayout newLayout,
+	uint32_t mipLevels)
     {
 	(void)format;
 	VkCommandBuffer commandBuffer = beginSingleTimeCommands();
@@ -527,7 +639,7 @@ private:
 	barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
 	barrier.image = image;
 	barrier.subresourceRange.baseMipLevel = 0;
-	barrier.subresourceRange.levelCount = 1;
+	barrier.subresourceRange.levelCount = mipLevels;
 	barrier.subresourceRange.baseArrayLayer = 0;
 	barrier.subresourceRange.layerCount = 1;
 
@@ -1286,7 +1398,8 @@ private:
 	    swapChainImageViews[i] = createImageView(
 		swapChainImages[i],
 		swapChainImageFormat,
-		VK_IMAGE_ASPECT_COLOR_BIT);
+		VK_IMAGE_ASPECT_COLOR_BIT,
+		1);
 	}
     }
 
